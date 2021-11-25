72d6649121cca8e7c94be8d9345ef02e7cf596c7
Albin-mca <albingeorge2023a@mca.ajce.in>
2021-11-26 01:55:56 +0530

    Eligibile for vote or not

---
+++ prgm14.py
@@ -0,0 +1,6 @@
+#WAPT to input an age and check whether the age is eligiblr to vote or not.
+age=int(input("Enter Your age :"))
+if(age>=18):
+    print("Eligible for vote")
+else :
+    print("not eligible for vote")
