cc6659217557dfe5d1b24bf4fc7c25f0147e032c
Albin-mca <albingeorge2023a@mca.ajce.in>
2021-11-26 02:01:44 +0530

    Even Numbers using for loop

---
+++ prgm19.py
@@ -0,0 +1,5 @@
+#19. Write a Program to print even numbers in a given sequence using for loop.
+a=[1,2,3,4,5,6,7,8,9,10]
+for numbers in a:
+    if(numbers % 2 == 0):
+        print("Even numbers are :",numbers)
